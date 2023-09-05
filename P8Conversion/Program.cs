10e7205d099476989aeaa283a7cb8fc360ae6477
sputgarin <sputgarin@protonmail.com>
2023-09-05 15:21:06 +0200

    Added P8Conversion Code

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+double doubleNumber;
+int intNumber;
+string input;
+
+Console.WriteLine("Give me a number.");
+input = Console.ReadLine();
+input = input.Replace(',', '.');
+doubleNumber = double.Parse(input, CultureInfo.InvariantCulture);
+//doubleNumber = double.Parse(input, CultureInfo.GetCultureInfo("sv-SE"));
+
+Console.WriteLine(doubleNumber);
+
+intNumber = Convert.ToInt32(doubleNumber);
+
+Console.WriteLine(intNumber);
