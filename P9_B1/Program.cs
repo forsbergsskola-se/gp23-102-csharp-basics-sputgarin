d8e921a808a277bb5ef5208a50bef3b367f4fb04
sputgarin <sputgarin@protonmail.com>
2023-09-07 11:09:30 +0200

    P9_B1 Completed

---
+++ P9_B1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Prompt the user for input.
+Console.WriteLine("Input a 0 or a 1");
+
+// Converting the user input to a int32 variable named number.
+int number = Convert.ToInt32(Console.ReadLine());
+
+/*
+Inverts the number to 1 if the input is 0. And changes the number to 0 if the input is 1
+How it works for input 1: 1 - 1 = 0 * -1 = 0
+How it works for input 0: 0 - 1 = -1 * -1 = 1. 
+*/
+number = (number - 1) * -1 ;
+
+// Print results.
+Console.WriteLine(number);
+
+// :)
