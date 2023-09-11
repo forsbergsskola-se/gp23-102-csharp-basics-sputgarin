cd31fb125249c0a83f2ea526bda233e439406115
sputgarin <sputgarin@protonmail.com>
2023-09-11 15:45:47 +0200

    P11_5EvenOrOdd Completed

---
+++ P11_5EvenOrOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿// Prompt the user for a number.
+Console.WriteLine("Please enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Check if number is even
+if (number % 2 == 0)
+{
+    Console.WriteLine("Number is even");
+}
+
+// If it's not even it is odd.
+else
+{
+    Console.WriteLine("Number is odd");
+}
+    
+    
