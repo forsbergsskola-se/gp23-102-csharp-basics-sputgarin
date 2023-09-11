b9d2c2c6b5a13126a1183aa17c13a0ad925ce9a8
sputgarin <sputgarin@protonmail.com>
2023-09-11 15:24:57 +0200

    P11_3Characters Improved

---
+++ P11_3Characters/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Prompt the user to enter a single character.
+Console.WriteLine("Please enter a single character: ");
+
+// 
+char userCharacter = Convert.ToChar(Console.ReadLine());
+
+string result = char.IsLetter(userCharacter) ?
+       (userCharacter == 'a' || userCharacter == 'e' || userCharacter == 'i' || userCharacter == 'o' ||
+       userCharacter == 'u' || userCharacter == 'A' || userCharacter == 'E' || userCharacter == 'I' ||
+       userCharacter == 'O' || userCharacter == 'U') ? 
+       "That's a vowel." 
+       : "That's a consonant" 
+       : "Not a letter";
+
+/*
+if (char.IsLetter(userCharacter))
+{
+    if (userCharacter == 'a' || userCharacter == 'e' || userCharacter == 'i' || userCharacter == 'o' ||
+        userCharacter == 'u' || userCharacter == 'A' || userCharacter == 'E' || userCharacter == 'I' ||
+        userCharacter == 'O' || userCharacter == 'U')
+    {
+        Console.WriteLine("That's a vowel.");  
+    }
+    else
+    {
+        Console.WriteLine("That's a consonant.");
+    }
+}
+
+else
+{
+      Console.WriteLine("You entered a digit.");  
+}
+
+*/
