4abc3e553d9680fead4b55fc735a0ae60b252314
sputgarin <sputgarin@protonmail.com>
2023-09-11 15:26:21 +0200

    P11_3Characters Added Comments

--- P11_3Characters/Program.cs
+++ P11_3Characters/Program.cs
@@ -5,6 +5,8 @@ Console.WriteLine("Please enter a single character: ");
 // 
 char userCharacter = Convert.ToChar(Console.ReadLine());
 
+// Check if the userCharacter is a digit and then type not a letter. 
+// Then checking if the letter is a vawel or a consonant.
 string result = char.IsLetter(userCharacter) ?
        (userCharacter == 'a' || userCharacter == 'e' || userCharacter == 'i' || userCharacter == 'o' ||
        userCharacter == 'u' || userCharacter == 'A' || userCharacter == 'E' || userCharacter == 'I' ||
