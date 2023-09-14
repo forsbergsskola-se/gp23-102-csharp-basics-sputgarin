6256758e300022232ca2fc398b3b8033db84388c
sputgarin <sputgarin@protonmail.com>
2023-09-14 11:22:07 +0200

    Completed P12_3Random_Item

---
+++ P12_3Random_Item/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Random random = new Random();
+int itemnumber = random.Next(1, 6);
+
+string response =   itemnumber == 1 ? "Accuired: Sword!" : 
+                    itemnumber == 2 ? "Accuired: Staff!" : 
+                    itemnumber == 3 ? "Accuired: Potion!" : 
+                    itemnumber == 4 ? "Accuired: Helmet" :
+                    "Accuired: Boots of spanish leather ";
+                    
+Console.WriteLine(response);                    
