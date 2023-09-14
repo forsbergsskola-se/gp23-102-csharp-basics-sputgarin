da7ee75fcb70b34e14f8a1c88981a271ca07013b
sputgarin <sputgarin@protonmail.com>
2023-09-14 11:33:23 +0200

    Completed P12_4Random_Chance

---
+++ P12_4Random_Chance/Program.cs
@@ -0,0 +1,8 @@
+﻿
+double secretMessageChance = 0.1f;
+double randomNumber = Random.Shared.NextDouble();
+
+string result = randomNumber < secretMessageChance
+    ? "Did you know alpacas can't spit?"
+    : "This is a normal boring message.";
+Console.WriteLine(result);
