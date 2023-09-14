acd5b7ff05cec4d202640275c359daf3b0f670b3
sputgarin <sputgarin@protonmail.com>
2023-09-14 09:12:02 +0200

    P13_2Triangle Completed
    :)

--- P13_2Triangle/Program.cs
+++ P13_2Triangle/Program.cs
@@ -1,32 +1,28 @@
 ﻿char symbol = '#';
 int counter = 0;
 int counterTwo = 0;
+int loopValue = 0;
 
 Console.WriteLine("How high do you want your pyramid to be?");
 int height = int.Parse(Console.ReadLine());
 
 OuterLoop:
-if (counter <= height)
+if (counter < height)
 {
+    
+    
     LoopInnerLoop:
-    if (counterTwo <= height)
+    if (counterTwo < height)
     {
+        
         counterTwo++;
         Console.Write(symbol);
         
         goto LoopInnerLoop;
         
     }
-    
-        if (counterTwo <= height)
-    {
-        counterTwo++;
-        Console.Write(symbol);
-        
-        goto LoopInnerLoop;
-        
-    }
-    
+    loopValue++;
+    counterTwo = loopValue;
     Console.WriteLine();
     
 
